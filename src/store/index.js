a10c9121fdd8506b8a8f72b566d2047cb0b89071
caochangkui <1243960844@qq.com>
2017-12-15 18:40:53 +0800

    添加商品至购物车

--- src/store/index.js
+++ src/store/index.js
@@ -5,25 +5,43 @@ Vue.use(Vuex)
 
 let store = new Vuex.Store({
   state: {
-    carPanalDate: [] // 数组中存放购物车的每个商品数据
+    carPanelData: [] // 数组中存放购物车的每个商品数据
+  },
+  getters: {
+    totalCount (state) { // 购物车商品总数
+      let count = 0
+      state.carPanelData.forEach((goods) => {
+        count += goods.count
+      })
+      return count
+    },
+    totalPrice (state) { // 购物车商品总价
+      let price = 0
+      state.carPanelData.forEach((goods) => {
+        price += goods.price * goods.count
+      })
+      return price
+    }
   },
   mutations: {
     addCarPanalData (state, data) {
       let bOff = true
-      state.carPanalDate.forEach((goods) => {
+      state.carPanelData.forEach((goods) => {
+        // 如果该商品已存在购物车，则直接使商品数量加 1
         if (goods.sku_id === data.sku_id) {
           goods.count++
           bOff = false
         }
       })
 
+      // 如果商品不存在，则添加到购物车
       if (bOff) {
-        let goodsDate = data
-        Vue.set(goodsDate, 'count', 1)
-        state.carPanalDate.push(goodsDate)
+        let goodsData = data
+        Vue.set(goodsData, 'count', 1) // 给对象goodsData设置属性 count，并赋值为 1
+        state.carPanelData.push(goodsData)
       }
 
-      console.log(state.carPanalDate)
+      console.log(state.carPanelData)
     }
   }
 })
